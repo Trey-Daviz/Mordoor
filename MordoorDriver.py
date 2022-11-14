7e51f26ea2c85187a93ea7ed508465cd45817a40
Trey_Daviz <treywesleydavis@gmail.com>
2022-11-14 15:47:05 -0600

    More functionality

--- MordoorDriver.py
+++ MordoorDriver.py
@@ -9,47 +9,47 @@ if __name__ == "__main__":
         sys.exit(v)
     system = ms.MordoorSystem(creds)
 
-    try:
-        system.add_door('IT2')
-    except Exception as e:
-        sys.exit(e)
-
-    try:
-        system.create_user('Test', 'User', 2, 'TestUser', 'TestPass')
-    except Exception as e:
-        sys.exit(e)
-
-    try:
-        system.create_dept('TestDept')
-    except Exception as e:
-        sys.exit(e)
-
-    try:
-        system.remove_door(6)
-    except Exception as e:
-        sys.exit(e)
-
-    try:
-        system.remove_dept(6)
-    except Exception as e:
-        sys.exit(e)
-
-    try:
-        system.add_door_to_elevated(1,2)
-    except Exception as e:
-        sys.exit(e)
-
-    try:
-        system.add_dept_to_user(1, 2)
-    except Exception as e:
-        sys.exit(e)
-
-    try:
-        system.add_dept_to_door(1, 2)
-    except Exception as e:
-        sys.exit(e)
-
-    try:
-        system.open_door(4)
-    except Exception as e:
-        sys.exit(e)
+    # try:
+    #     system.add_door('IT2')
+    # except Exception as e:
+    #     sys.exit(e)
+    #
+    # try:
+    #     system.create_user('Test', 'User', 2, 'TestUser', 'TestPass')
+    # except Exception as e:
+    #     sys.exit(e)
+    #
+    # try:
+    #     system.create_dept('TestDept')
+    # except Exception as e:
+    #     sys.exit(e)
+    #
+    # try:
+    #     system.remove_door(6)
+    # except Exception as e:
+    #     sys.exit(e)
+    #
+    # try:
+    #     system.remove_dept(6)
+    # except Exception as e:
+    #     sys.exit(e)
+    #
+    # try:
+    #     system.add_door_to_elevated(1,2)
+    # except Exception as e:
+    #     sys.exit(e)
+    #
+    # try:
+    #     system.add_dept_to_user(1, 2)
+    # except Exception as e:
+    #     sys.exit(e)
+    #
+    # try:
+    #     system.add_dept_to_door(1, 2)
+    # except Exception as e:
+    #     sys.exit(e)
+    #
+    # try:
+    #     system.open_door(4)
+    # except Exception as e:
+    #     sys.exit(e)
